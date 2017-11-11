b65856c5f61cf89f1fcea2b00e17ece302780589
jmurp7385 <jmurp7385@gmail.com>
2017-11-11 14:21:02 -0500

    speech stuff

--- app/app.py
+++ app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask,render_template
 import io
 import os
 
@@ -13,33 +13,45 @@ os.environ["GOOGLE_APPLICATION_CREDENTIALS"]='runa_credentials.json'
 # Instantiates a client
 client = speech.SpeechClient()
 
-# The name of the audio file to transcribe
-file_name = os.path.join(
-    os.path.dirname(__file__),
-    'resources',
-    'audio.raw')
-
-# Loads the audio into memory
-with io.open(file_name, 'rb') as audio_file:
-    content = audio_file.read()
-    audio = types.RecognitionAudio(content=content)
-
 config = types.RecognitionConfig(
-    encoding=enums.RecognitionConfig.AudioEncoding.LINEAR16,
-    sample_rate_hertz=16000,
-    language_code='en-US')
-
-# Detects speech in the audio file
-response = client.recognize(config, audio)
-
-for result in response.results:
-    print('Transcript: {}'.format(result.alternatives[0].transcript))
+  encoding=enums.RecognitionConfig.AudioEncoding.LINEAR16,
+  sample_rate_hertz=16000,
+  language_code='en-US')
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-  return url_for('index.html')
+  return render_template('index.html')
+
+
+@app.route('/save_transcript')
+def save_transcript():
+  # The name of the audio file to transcribe
+  file_name = os.path.join(
+      os.path.dirname(__file__),
+      'resources',
+      'audio.raw')
+
+  # Loads the audio into memory
+  with io.open(file_name, 'rb') as audio_file:
+      content = audio_file.read()
+      audio = types.RecognitionAudio(content=content)
+  # Detects speech in the audio file
+  response = client.recognize(config, audio)
+
+  for result in response.results:
+    print('Transcript: {}'.format(result.alternatives[0].transcript))
+
+@app.route('/end_conversation', methods=['GET'])
+def end_conversation():
+
+  return
+
+@app.route('/progress', methods=['GET'])
+def progress():
+
+  return
 
 if __name__=='__main__':
   app.run(debug=True)
